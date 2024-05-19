114de1d953dcb94aef82a22e2361f134e0d24fbb
B. Finch M. <benjaminfinch@skiff.com>
2024-05-19 03:50:01 -0400

    done

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,43 +1,85 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			store: {
+				contacts: [],
+				userToEdit: {},
+				editing: false
 			},
+		actions: {
+			getContacts: () => {
+				fetch('https://playground.4geeks.com/contact/agendas/benja/contacts')
+				.then( (response) => response.json())
+				.then( data => setStore({ contacts: data.contacts }))	
+			},
+
+			createContact: (name, phone, email, address) => {
+				const requestOptions = {
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify({ 
+						"name": name,
+						"phone": phone,
+						"email": email,
+						"address": address
+					 })
+				};
+				fetch('https://playground.4geeks.com/contact/agendas/benja/contacts', requestOptions)
+					.then(response => response.json())
+					.then(() => getActions().getContacts())
+			.catch(error => console.error('Hubo un error al realizar la solicitud:', error));
+			},
+
+			deleteContact: (iDToDelete) => {
+				fetch(`https://playground.4geeks.com/contact/agendas/benja/contacts/${iDToDelete}`, { method: 'DELETE' })
+				.then( () => getActions().getContacts())
+			},
+
+			setEditing: (value) => {
+				setStore({ editing: value });
+			  },
+
+			  modifyContacts: (iDSelected) => {
+				const userSelected = getStore().contacts.find(user => user.id === iDSelected)
+				setStore({ userToModify: userSelected })
+				setStore({editing: true })
+			},
+			modifyContactAPI: (name, phone, email, address, iDToEdit) => {
+				const requestOptions = {
+					method: 'PUT',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify({ 
+						"name": name,
+						"phone": phone,
+						"email": email,
+						"address": address
+					 })
+				};
+				fetch(`https://playground.4geeks.com/contact/agendas/benja/contacts/${iDToEdit}`, requestOptions)
+					.then(response => response.json())
+					.then( setStore({ userToModify: {} }))
+					.then(() => getActions().getContacts())
+			},
+			searchMyUser : () => {
+				fetch("https://playground.4geeks.com/contact/agendas?offset=0&limit=100")
+				.then( (response) => response.json())
+				.then( data => {
+					const myUser = data.agendas.find (user => user.slug === "benja")
+					if(!myUser) {
+							fetch(`"https://playground.4geeks.com/contact/agendas/benja"`, { method: 'POST' })
+							.then((response) => response.json())
+							.then( () => getActions().getContacts())}
+				})
+			},
+
 			loadSomeData: () => {
+
+				getActions().searchMyUser()
+				getActions().getContacts();
+				console.log(getStore().contacts)
 				/**
 					fetch().then().then(data => setStore({ "foo": data.bar }))
 				*/
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
 		}
 	};
 };
